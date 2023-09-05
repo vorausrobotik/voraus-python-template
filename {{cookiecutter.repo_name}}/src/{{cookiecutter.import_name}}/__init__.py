2320c4428d8b93d80bfab261ce292207a46afbba
Philipp Caspers <117186241+philipp-caspers@users.noreply.github.com>
2023-09-05 09:02:18 +0200

    style: Format with black (#59)
    I think it is nice if the template is already properly formatted and not
    only relying on the post gen hook.
    
    Co-authored-by: Jan-Frederik Schmidt <jan-frederik.schmidt@vorausrobotik.com>

--- {{cookiecutter.repo_name}}/src/{{cookiecutter.import_name}}/__init__.py
+++ {{cookiecutter.repo_name}}/src/{{cookiecutter.import_name}}/__init__.py
@@ -7,9 +7,9 @@ from importlib_metadata import PackageNotFoundError, version
 
 __module_name__ = "{{ cookiecutter.import_name }}"
 
-try: # pragma: no cover
+try:  # pragma: no cover
     __version__ = version(__module_name__)
-except PackageNotFoundError as error: # pragma: no cover
+except PackageNotFoundError as error:  # pragma: no cover
     raise ModuleNotFoundError(
         f"Unable to determine version of package '{__module_name__}'. "
         "If you are on a local development system, use 'pip install -e .[dev]' in order to install the package. "
--- {{cookiecutter.repo_name}}/tests/unit/test_example.py
+++ {{cookiecutter.repo_name}}/tests/unit/test_example.py
@@ -11,5 +11,6 @@ from {{cookiecutter.import_name}}.example import my_function
 def test_my_function() -> None:
     assert my_function() == 42
 
+
 def test_use_test_resource(resource_dir: Path) -> None:
     assert resource_dir.joinpath("example_resource.txt").is_file()
